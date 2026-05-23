c9081a9559d8eeed18ad9a3971262400b10d3f09
OG-DEO <YOUR_GITHUB_ID+OG-DEO@users.noreply.github.com>
2026-05-23 13:34:06 -0700

    Track branch creation status

--- orchestrators/branch_stage.py
+++ orchestrators/branch_stage.py
@@ -1,12 +1,23 @@
 from core.task_branch_manager import TaskBranchManager
+from core.branch_status_index import BranchStatusIndex
 
 
 class BranchStage:
     def __init__(self):
         self.manager = TaskBranchManager()
+        self.status_index = BranchStatusIndex()
 
     def run(self, task):
         branch = self.manager.create_for_task(task)
+
+        self.status_index.set_status(
+            branch,
+            "executing",
+            {
+                "task_id": task.get("id", "unknown"),
+                "stage": "branch_created",
+            },
+        )
 
         return {
             "branch": branch
